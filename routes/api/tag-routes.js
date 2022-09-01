861b57ac08d6ec3a11a7f81ea56b9e43d0cc5364
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-09-01 19:54:24 -0400

    fixed post and put for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,37 +48,23 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
-  Product.create({
-    product_name: req.body.produce_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.category_id,
-    tagIds: req.body.tag_id
-})
-  .then((product) => {
-    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map((tag_id) => {
-        return {
-          product_id: product.id,
-          tag_id,
-        };
-      });
-      return ProductTag.bulkCreate(productTagIdArr);
-    }
-    // if no product tags, just respond
-    res.status(200).json(product);
+  Tag.create({
+    tag_name: req.body.tag_name,
   })
-  .then((productTagIds) => res.status(200).json(productTagIds))
-  .catch((err) => {
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
     console.log(err);
-    res.status(400).json(err);
+    res.status(500).json(err);
   });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(req.body, {
+  Tag.update(
+    {
+    tag_name: req.body.tag_name,
+    }, 
+    {
     where: {
       id: req.params.id,
     },
